17362a6af7b26be58ad71df0934f1471980e0b3a
pifragile <pigu69@gmail.com>
2019-05-02 23:03:34 +0200

    switch to token authentication as sessions and csrf are a fucking mess when using different domains for FE and BE

--- src/api/session.js
+++ src/api/session.js
@@ -1,14 +1,8 @@
 import axios from 'axios';
 import config from '../../config';
 
-const CSRF_COOKIE_NAME = 'csrftoken';
-const CSRF_HEADER_NAME = 'X-CSRFToken';
-
 const session = axios.create({
-  xsrfCookieName: CSRF_COOKIE_NAME,
-  xsrfHeaderName: CSRF_HEADER_NAME,
   baseURL: config.backendUrl,
-  withCredentials: true,
 });
 
 export default session;
