feae66543da3d64adb9107b8a0cc1c19cc7d2e5a
pifragile <pigu@bluewin.ch>
2021-06-09 09:17:35 +0200

    simplify direct jumping to anchors on page load

--- src/views/Intern.vue
+++ src/views/Intern.vue
@@ -6,39 +6,26 @@
       :domId="index"
       :key="concert.id"
       :concert="concert"
-      :userId="userId"></Concert>
+      :userId="userId"
+      :ref="'anchor-' + concert.id"></Concert>
   </div>
 </template>
 
 <script>
-import Concert from '../components/Concert';
-import concerts from '../api/concerts';
-import auth from '../api/auth';
+import Concert from '../components/Concert'
+import concerts from '../api/concerts'
+import auth from '../api/auth'
 
 export default {
   name: 'home',
   updated() {
-    // can use anchor-<concert-id> to directly jump to the specified concert
-    // this is done because anchors are not working on initial page load
+    // this is done because anchors do not work on initial page load,
+    // so if there is an anchor to a concert we find the corresponding
+    // child component and jump there directly
     const section = window.location.hash.replace('#', '')
-    console.log(this.$children)
     if (section) {
-      const split = section.split('anchor-')
-      if (split.length > 1) {
-        const key = parseInt(split[1])
-        let el = null
-        for (let i = 0; i < this.$children.length; i++){
-          const child = this.$children[i]
-          console.log(child.$vnode.key)
-          if (child.$vnode.key === key) {
-            el = child.$el
-            break
-          }
-        }
-
-        if (el) {
-          this.$nextTick(() => el.scrollIntoView())
-        }
+      if (this.$refs[section]) {
+        this.$nextTick(() => this.$refs[section][0].$el.scrollIntoView())
       }
     }
   },
@@ -49,20 +36,20 @@ export default {
     return {
       concerts: [],
       userId: null,
-    };
+    }
   },
   async beforeCreate() {
     this.concerts = (await concerts.listConcerts())
       .data
-      .sort((a, b) => new Date(b.date) - new Date(a.date));
-    this.userId = (await auth.getAccountDetails()).data.pk;
+      .sort((a, b) => new Date(b.date) - new Date(a.date))
+    this.userId = (await auth.getAccountDetails()).data.pk
   },
   methods: {
     removeConcert(id) {
-      this.concerts = this.concerts.filter(concert => concert.id !== id);
+      this.concerts = this.concerts.filter(concert => concert.id !== id)
     },
   },
-};
+}
 </script>
 
 <style>
